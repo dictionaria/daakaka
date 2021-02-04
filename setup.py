82d4f9fac29953d63a21b3267bec46ec525db296
Johannes Englisch <englisch@shh.mpg.de>
2021-02-04 12:33:56 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_daakaka',
+    py_modules=['cldfbench_daakaka'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'daakaka=cldfbench_daakaka:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+        'pydictionaria'
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
